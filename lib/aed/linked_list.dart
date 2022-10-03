edbbea486ab64425fd923443d05fb4d000901d36
Douglas Ceron <ceron.douglas@gmail.com>
2022-10-03 14:07:51 -0300

    adding linked list

--- lib/aed/linked_list.dart
+++ lib/aed/linked_list.dart
@@ -1,1 +1,113 @@
-class LinkedList {}
+void main(List<String> args) {
+  final list = LinkedList<int>();
+  list.push(3);
+  list.push(2);
+  list.push(1);
+
+  print('Before: $list');
+
+  final firstNode = list.nodeAt(0);
+  final removedValue = list.removeAfter(firstNode!);
+
+  print('After:  $list');
+  print('Removed value: $removedValue');
+}
+
+class Node<T> {
+  Node({required this.value, this.next});
+  T value;
+  Node<T>? next;
+
+  @override
+  String toString() {
+    if (next == null) {
+      return '$value';
+    }
+    return '$value -> $next';
+  }
+}
+
+class LinkedList<E> {
+  Node<E>? head;
+  Node<E>? tail;
+
+  @override
+  bool get isEmpty => head == null;
+
+  @override
+  String toString() {
+    if (isEmpty) return 'Empty list';
+    return head.toString();
+  }
+
+  //Inserting Values
+  void push(E value) {
+    head = Node(value: value, next: head);
+    tail ??= head;
+  }
+
+  void append(E value) {
+    if (isEmpty) {
+      push(value);
+      return;
+    }
+
+    tail!.next = Node(value: value);
+
+    tail = tail!.next;
+  }
+
+  Node<E>? nodeAt(int index) {
+    var currentNode = head;
+    var currentIndex = 0;
+
+    while (currentNode != null && currentIndex < index) {
+      currentNode = currentNode.next;
+      currentIndex += 1;
+    }
+    return currentNode;
+  }
+
+  Node<E> insertAfter(Node<E> node, E value) {
+    if (tail == node) {
+      append(value);
+      return tail!;
+    }
+
+    node.next = Node(value: value, next: node.next);
+    return node.next!;
+  }
+
+  //Removing values
+  E? pop() {
+    final value = head?.value;
+    head = head?.next;
+    if (isEmpty) {
+      tail = null;
+    }
+    return value;
+  }
+
+  E? removeLast() {
+    if (head?.next == null) return pop();
+
+    var current = head;
+    while (current!.next != tail) {
+      current = current.next;
+    }
+
+    final value = tail?.value;
+    tail = current;
+    tail?.next = null;
+    return value;
+  }
+
+  E? removeAfter(Node<E> node) {
+    final value = node.next?.value;
+    if (node.next == tail) {
+      tail = node;
+    }
+    node.next = node.next?.next;
+    return value;
+  }
+}
