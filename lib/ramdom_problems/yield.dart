518727679fbce31fa4723f589b9af0a368ab3851
Douglas Ceron <ceron.douglas@gmail.com>
2022-10-17 18:02:11 -0300

    refactoring yield

--- lib/ramdom_problems/yield.dart
+++ lib/ramdom_problems/yield.dart
@@ -9,25 +9,37 @@ Iterable<int> countIterable(int num) sync* {
   }
 }
 
-Stream<int> countStream(int num) async* {
-  for (var i = 0; i < num; i++) {
+//async* yield will return a Stream<dynamic>
+Stream<int> runToMax(int n) async* {
+  int i = 0;
+  while (i < n) {
     yield i;
-    sleep(Duration(seconds: 1));
+    i++;
+    await Future.delayed(Duration(seconds: 300));
   }
 }
 
-Iterable<int> getRange(int start, int finish) sync* {
-  if (start <= finish) {
-    yield start;
-    for (final value in getRange(start + 1, finish)) {
-      yield value;
-    }
+//async* yield* will call a function and return Stream<dynamic>
+Stream<int> countDownAsyncRecursive(int n) async* {
+  if (n > 0) {
+    yield n;
+    yield* countDownAsyncRecursive(n - 1);
   }
 }
 
-Iterable<int> getRangeYield(int start, int finish) sync* {
-  if (start <= finish) {
-    yield start;
-    yield* getRange(start + 1, finish);
+//sync* yield will return an Iterable<dynamic>
+Iterable<int> genIterates(int max) sync* {
+  var i = 0;
+  while (i < max) {
+    yield i;
+    i++;
+  }
+}
+
+//sync* yield* will call a function and return Iterable<dynamic>
+Iterable<int> countDownSyncRecursive(int n) sync* {
+  if (n > 0) {
+    yield n;
+    yield* countDownSyncRecursive(n - 1);
   }
 }
